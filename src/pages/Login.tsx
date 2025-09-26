545d05f6d7f5e979d9117c05305cf799b22d73ad
darde55 <nick.bayer@gmx.de>
2025-09-26 15:54:19 +0200

    Initiales Frontend für TSV Wolfschlugen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -21,8 +21,8 @@ const Login: React.FC = () => {
   const handleLogin = async () => {
     try {
       setError("");
-      // Korrigierte API-URL:
-      const res = await api.post("/api/login", { username, password });
+      // Korrigierte API-URL (nur /login, da baseURL schon /api ist)
+      const res = await api.post("/login", { username, password });
       setUser({
         username: res.data.username,
         role: res.data.role,
