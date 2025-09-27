4ae29407bf5ff3fcd3f7f3862998843da414b692
darde55 <nick.bayer@gmx.de>
2025-09-27 18:23:57 +0200

    Neue Admin Seite2

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -3,27 +3,23 @@ import { Container, Typography, Paper, Box, Divider } from "@mui/material";
 import UserAdmin from "../components/UserAdmin";
 import TermineAdmin from "../components/TermineAdmin";
 
-const Admin: React.FC = () => {
-  return (
-    <Container sx={{ mt: 4 }}>
-      <Paper sx={{ p: 4 }}>
-        <Typography variant="h5" mb={2}>
-          Admin-Bereich
-        </Typography>
-        <Typography mb={3}>
-          Hier kannst du Termine und Benutzer verwalten!
-        </Typography>
-        <Divider sx={{ mb: 3 }} />
-        <Box mb={4}>
-          <UserAdmin />
-        </Box>
-        <Divider sx={{ mb: 3 }} />
-        <Box>
-          <TermineAdmin />
-        </Box>
-      </Paper>
-    </Container>
-  );
-};
+const Admin: React.FC = () => (
+  <Container sx={{ mt: 4 }}>
+    <Paper sx={{ p: 4 }}>
+      <Typography variant="h5" mb={2}>Admin-Bereich</Typography>
+      <Typography mb={3}>
+        Hier kannst du Termine und Benutzer verwalten!
+      </Typography>
+      <Divider sx={{ mb: 3 }} />
+      <Box mb={4}>
+        <UserAdmin />
+      </Box>
+      <Divider sx={{ mb: 3 }} />
+      <Box>
+        <TermineAdmin />
+      </Box>
+    </Paper>
+  </Container>
+);
 
 export default Admin;
